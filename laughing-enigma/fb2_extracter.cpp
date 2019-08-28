a6b1938f9431aa2c4abb806823d40a826f3c7142
CleverShovel <37588466+CleverShovel@users.noreply.github.com>
2019-08-29 00:01:13 +0400

    Some changes in code structure

--- laughing-enigma/fb2_extracter.cpp
+++ laughing-enigma/fb2_extracter.cpp
@@ -1,82 +1,264 @@
 #include "fb2_extracter.h"
-#include "fmt/format.h"
 
 #include <QDebug>
 
-#include <string>
-#include <unordered_map>
-#include <variant>
+#include <unordered_set>
 
 using namespace std;
 
-namespace FB2Parser {
-Base::Base(QXmlStreamReader& xml) : xml(xml) {}
+namespace FB2Parse {
+const unordered_map <QString, decltype(&Create<Base>)> tags = {
+  {"description",   &Create<Description>},
+  {"title",         &Create<Title>},
+  {"image",         &Create<Image>},
+  {"a",             &Create<A>},
+  {"table",         &Create<Table>},
+  {"tr",            &Create<Tr>},
+  {"td",            &Create<Td>},
+  {"th",            &Create<Th>},
+  {"sub",           &Create<Sub>},
+  {"sup",           &Create<Sup>},
+  {"code",          &Create<Code>},
+  {"strong",        &Create<Strong>},
+  {"cite",          &Create<Cite>},
+  {"empty-line",    &Create<EmptyLine>},
+  {"emphasis",      &Create<Emphasis>},
+  {"strikethrough", &Create<Strikethrough>},
+  {"default",       &Create<Redunant>}
+};
 
-Document::Document(QXmlStreamReader& xml) : Base(xml) {}
+Reader::Reader(QXmlStreamReader* xml) : xml(xml) {}
 
-void Document::Parse(QString& str) {
-  str.reserve(str.size() + 50);
+QString arg_variable_size(QString fmt, std::vector<QString> args) {
+  return std::accumulate(
+             std::make_move_iterator(args.begin()),
+             std::make_move_iterator(args.end()),
+             std::move(fmt),
+  [](QString toFmt, QString arg) {
+    return toFmt.arg(arg);
+  }
+         );
+}
+
+QString Reader::Parse() {
+  ReadTokens();
+
+  return arg_variable_size(move(str), move(args));
+}
+
+void Reader::ReadTokens() {
+  while (!xml->atEnd()) {
+    auto token = xml->readNext();
+    if (token == QXmlStreamReader::StartDocument) {
+      elems.emplace_back(new Document(*this));
+      elems.back()->Start(str);
+    }
+    if (token == QXmlStreamReader::EndDocument) {
+      elems.back()->End(str);
+      elems.pop_back();
+    }
+    if (token == QXmlStreamReader::StartElement) {
+      auto tag = xml->name().toString();
+      if (tags.count(tag))
+        elems.push_back(tags.at(tag)(*this));
+      else
+        elems.push_back(tags.at("default")(*this));
+      elems.back()->Start(str);
+    }
+    if (token == QXmlStreamReader::EndElement) {
+      if (elems.back()->GetTag() == xml->name().toString() ||
+          elems.back()->GetTag() == "") {
+        elems.back()->End(str);
+        elems.pop_back();
+      }
+    }
+    if (token == QXmlStreamReader::Characters) {
+      if (xml->text().toString().contains(QRegExp("[A-Z]|[a-z]|[А-Я]|[а-я]"))) {
+        str += "<p>" + xml->text().toString() + "</p>";
+      }
+    }
+  }
+}
+
+QXmlStreamReader& Reader::GetXml() { return *xml; }
+
+void Reader::AddArg(QString arg) {
+  args.push_back(move(arg));
+}
+
+std::unordered_map<QString, int>& Reader::GetImages() {
+  return images;
+}
+
+Base::Base(Reader& reader, QString tag = "") : reader(reader), tag(move(tag)) {}
+
+void Base::Start(QString&) {}
+void Base::End(QString&) {}
+const QString& Base::GetTag() const { return tag; }
+
+Document::Document(Reader& reader) : Base(reader, "FictionBook") {}
+
+void Document::Start(QString& str) {
+  str.reserve(str.size() + 60);
   str += "<!DOCTYPE HTML><html><body style=\"font-size:";
-  str += to_string(font_size).c_str();
+  str += QString::number(text_font_size);
   str += "px\">";
 }
-} // FB2Parser
+
+void Document::End(QString& str) {
+  str += "</body></html>";
+}
+
+Description::Description(Reader& reader) : Tag(reader, "description") {}
+//TitleInfo::TitleInfo(Reader& reader) : Base(reader) {}
+
+Tag::Tag(Reader& reader, QString tag) : Base(reader, move(tag)) {}
+
+void Tag::Start(QString& str) {
+  str.reserve(str.size() + 2 + tag.size());
+  str += "<";
+  str += tag;
+  str += ">";
+}
+
+void Tag::End(QString& str) {
+  str.reserve(str.size() + 3 + tag.size());
+  str += "</";
+  str += tag;
+  str += ">";
+}
+
+Body::Body(Reader& reader) : Base(reader, "body") {}
+
+P::P(Reader& reader)                            : Tag(reader, "p") {}
+A::A(Reader& reader)                            : Tag(reader, "a") {}
+Table::Table(Reader& reader)                    : Tag(reader, "table") {}
+Tr::Tr(Reader& reader)                          : Tag(reader, "tr") {}
+Td::Td(Reader& reader)                          : Tag(reader, "td") {}
+Th::Th(Reader& reader)                          : Tag(reader, "th") {}
+Sub::Sub(Reader& reader)                        : Tag(reader, "sub") {}
+Sup::Sup(Reader& reader)                        : Tag(reader, "sup") {}
+Code::Code(Reader& reader)                      : Tag(reader, "code") {}
+Strong::Strong(Reader& reader)                  : Tag(reader, "strong") {}
+Cite::Cite(Reader& reader)                      : Tag(reader, "cite") {}
+
+EmptyLine::EmptyLine(Reader& reader)            : Tag(reader, "br") {}
+Emphasis::Emphasis(Reader& reader)              : Tag(reader, "i") {}
+Strikethrough::Strikethrough(Reader& reader)    : Tag(reader, "strike") {}
+
+Image::Image(Reader& reader) : Tag(reader, "image") {}
+
+void Image::Start(QString& str) {
+  if (reader.GetXml().attributes().count() > 0) {
+    auto attr = reader.GetXml().attributes().at(0).value().toString();
+    attr.remove(0, 1);
+    auto& images = reader.GetImages();
+    if (images.count(attr) == 0)
+      images[attr] = images.size();
+    str.reserve(str.size() + 23 + attr.size());
+    str += "<p align=\"center\">%";
+    str += QString::number(images[attr]);
+    str += "</p>";
+  }
+}
+
+void Image::End(QString&) {
+}
+
+Title::Title(Reader& reader) : Tag(reader, "title") {}
+
+void Title::Start(QString& str) {
+  str.reserve(str.size() + 50);
+  qDebug() << QString("<p align=\"center\" style=\"font-size:") +
+           QString::number(title_font_size) + "px\">";
+  str += "<p align=\"center\" style=\"font-size:";
+  str += QString::number(title_font_size);
+  str += "px\">";
+}
+
+void Title::End(QString& str) {
+  str += "</p>";
+}
+
+Binary::Binary(Reader& reader) : Tag(reader, "binary") {}
+
+void Binary::Start(QString&) {
+//  TODO: change this
+//  auto& xml = reader.GetXml();
+//  QString id = xml.attributes().at(0).value().toString();
+//  QString type = xml.attributes().at(1).value().toString();
+//  xml.readNext();
+//  QString value;
+//  value.reserve(250);
+//  value = "<img src=\"data:" +
+//          std::move(type) +
+//          ";base64," +
+//          xml.text().toString()
+//          + "\"/>";
+//  reader.AddArg(move(value));
+}
+
+void Binary::End(QString&) {}
+
+Redunant::Redunant(Reader& reader) : Base(reader) {}
+} // FB2Parse
 
 
 FB2Extracter::FB2Extracter(QFile* file) {
   QXmlStreamReader xml(file);
-  QStringList thisToken;
-  QString imgId, imgType;
-  while (!xml.atEnd()) {
-    switch (xml.readNext()) {
-      case QXmlStreamReader::NoToken:
-        break;
-      case QXmlStreamReader::StartDocument:
-        book_ = "<!DOCTYPE HTML><html><body style=\"font-size:14px\">";
-        break;
-      case QXmlStreamReader::EndDocument:
-        book_.append("</body></html>");
-        break;
-      case QXmlStreamReader::StartElement:
-        thisToken.append(xml.name().toString());
-        if (xml.name().toString() == "") {
+  FB2Parse::Reader reader(&xml);
+  book_ = reader.Parse();
+//  QStringList thisToken;
+//  QString imgId, imgType;
+//  while (!xml.atEnd()) {
+//    switch (xml.readNext()) {
+//      case QXmlStreamReader::NoToken:
+//        break;
+//      case QXmlStreamReader::StartDocument:
+//        book_ = "<!DOCTYPE HTML><html><body style=\"font-size:14px\">";
+//        break;
+//      case QXmlStreamReader::EndDocument:
+//        book_.append("</body></html>");
+//        break;
+//      case QXmlStreamReader::StartElement:
+//        thisToken.append(xml.name().toString());
+//        if (xml.name().toString() == "") {
 
-        }
-        if (xml.name().toString() == "image") { // расположение рисунков
-          if (xml.attributes().count() > 0)
-            book_.append("<p align=\"center\">" + xml.attributes().at(0).value().toString() + "</p>");
-        }
-        if (xml.name() == "binary") { // хранилище рисунков
-          imgId = xml.attributes().at(0).value().toString();
-          imgType = xml.attributes().at(1).value().toString();
-        }
-        break;
-      case QXmlStreamReader::EndElement:
-        if (thisToken.last() == xml.name().toString())
-          thisToken.removeLast();
-        break;
-      case QXmlStreamReader::Characters:
-        if (xml.text().toString().contains(QRegExp("[A-Z]|[a-z]|[А-Я]|[а-я]"))) { // если есть текст в блоке
-          if (thisToken.contains("description")) { // ОПИСАНИЕ КНИГИ
-            break; // не выводим
-          }
-          if (thisToken.contains("div"))
-            break;
-          if (!thisToken.contains("binary"))
-            book_.append("<p>" + xml.text().toString() + "</p>");
-        }
-        if (thisToken.contains("binary")) { //для рисунков
-          qDebug() << "read image";
-          QString image = "<img src=\"data:"
-                          + imgType + ";base64,"
-                          + xml.text().toString()
-                          + "\"/>";
-          book_.replace("#" + imgId, image);
-        }
-        break;
-      default:
-        break;
-    }
+//        }
+//        if (xml.name().toString() == "image") { // расположение рисунков
+//          if (xml.attributes().count() > 0)
+//            book_.append("<p align=\"center\">" + xml.attributes().at(0).value().toString() + "</p>");
+//        }
+//        if (xml.name() == "binary") { // хранилище рисунков
+//          imgId = xml.attributes().at(0).value().toString();
+//          imgType = xml.attributes().at(1).value().toString();
+//        }
+//        break;
+//      case QXmlStreamReader::EndElement:
+//        if (thisToken.last() == xml.name().toString())
+//          thisToken.removeLast();
+//        break;
+//      case QXmlStreamReader::Characters:
+//        if (xml.text().toString().contains(QRegExp("[A-Z]|[a-z]|[А-Я]|[а-я]"))) { // если есть текст в блоке
+//          if (thisToken.contains("description")) { // ОПИСАНИЕ КНИГИ
+//            break; // не выводим
+//          }
+//          if (thisToken.contains("div"))
+//            break;
+//          if (!thisToken.contains("binary"))
+//            book_.append("<p>" + xml.text().toString() + "</p>");
+//        }
+//        if (thisToken.contains("binary")) { //для рисунков
+//          QString image = "<img src=\"data:"
+//                          + imgType + ";base64,"
+//                          + xml.text().toString()
+//                          + "\"/>";
+//          book_.replace("#" + imgId, image);
+//        }
+//        break;
+//      default:
+//        break;
+//    }
 
-  }
+//  }
 }
--- laughing-enigma/fb2_extracter.h
+++ laughing-enigma/fb2_extracter.h
@@ -4,33 +4,187 @@
 
 #include <QFile>
 #include <QXmlStreamReader>
+#include <QString>
+#include <QStringView>
 
 #include <string>
+#include <string_view>
+#include <vector>
+#include <memory>
+#include <unordered_map>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iterator>
 
-namespace FB2Parser {
-const int font_size = 14;
+namespace std {
+template<> struct hash<QString> {
+  std::size_t operator()(const QString& s) const {
+    return qHash(s);
+  }
+};
+}
+
+namespace FB2Parse {
+class Base;
+const int title_font_size = 20;
+const int text_font_size = 14;
+
+class Reader {
+ public:
+  Reader() = default;
+  Reader(QXmlStreamReader*);
+  QString Parse();
+  void ReadTokens();
+  QXmlStreamReader& GetXml();
+  void AddArg(QString);
+  std::unordered_map<QString, int>& GetImages();
+ private:
+  QString str;
+  QXmlStreamReader* xml;
+  std::vector<std::unique_ptr<Base>> elems;
+  std::unordered_map<QString, int> images;
+  std::vector<QString> args;
+};
+
+template <typename T>
+std::unique_ptr<Base> Create(Reader& reader) {
+  return std::make_unique<T>(reader);
+}
+
 class Base {
  public:
-  Base(QXmlStreamReader&);
-
-  virtual void Start(QString&) = 0;
-  virtual void End(QString&) = 0;
+  Base(Reader&, QString);
+  virtual void Start(QString&);
+  virtual void End(QString&);
   virtual ~Base() = default;
+  virtual const QString& GetTag() const;
 
  protected:
-  QXmlStreamReader& xml;
+  Reader& reader;
+  QString tag;
 };
 
 class Document : public Base {
  public:
-  Document(QXmlStreamReader& xml);
-
+  Document(Reader&);
   void Start(QString&) override;
   void End(QString&) override;
-
-  ~Document() override = default;
 };
-} // namespace FB2Parser
+
+class Tag : public Base {
+ public:
+  Tag(Reader&, QString);
+  void Start(QString&) override;
+  void End(QString&) override;
+};
+
+class Body : public Base {
+ public:
+  Body(Reader&);
+};
+
+class Description : public Tag {
+ public:
+  Description(Reader&);
+};
+
+class P : public Tag {
+ public:
+  P(Reader&);
+};
+
+class A : public Tag {
+ public:
+  A(Reader&);
+};
+
+class Table : public Tag {
+ public:
+  Table(Reader&);
+};
+
+class Tr : public Tag {
+ public:
+  Tr(Reader&);
+};
+
+class Td : public Tag {
+ public:
+  Td(Reader&);
+};
+
+class Th : public Tag {
+ public:
+  Th(Reader&);
+};
+
+class Sub : public Tag {
+ public:
+  Sub(Reader&);
+};
+
+class Sup : public Tag {
+ public:
+  Sup(Reader&);
+};
+
+class Code : public Tag {
+ public:
+  Code(Reader&);
+};
+
+class Strong : public Tag {
+ public:
+  Strong(Reader&);
+};
+
+class Cite : public Tag {
+ public:
+  Cite(Reader&);
+};
+
+class EmptyLine : public Tag {
+ public:
+  EmptyLine(Reader&);
+};
+
+class Emphasis : public Tag {
+ public:
+  Emphasis(Reader&);
+};
+
+class Strikethrough : public Tag {
+ public:
+  Strikethrough(Reader&);
+};
+
+class Image : public Tag {
+ public:
+  Image(Reader&);
+  void Start(QString&) override;
+  void End(QString&) override;
+};
+
+class Title : public Tag {
+ public:
+  Title(Reader&);
+  void Start(QString&) override;
+  void End(QString&) override;
+};
+
+class Binary : public Tag {
+ public:
+  Binary(Reader&);
+  void Start(QString&) override;
+  void End(QString&) override;
+};
+
+class Redunant : public Base {
+ public:
+  Redunant(Reader&);
+};
+} // namespace FB2Parse
 
 class FB2Extracter : public Extracter {
  public:
